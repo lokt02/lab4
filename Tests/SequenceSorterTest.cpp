24d0072c7690ee381cdd74ca3dac37b6fba08957
Sergei <lokt02@mail.ru>
2021-10-05 14:58:05 +0300

    Убрал Хипсорт из тестов

--- Tests/SequenceSorterTest.cpp
+++ Tests/SequenceSorterTest.cpp
@@ -139,72 +139,72 @@ TEST(InsertionSort, StringList){
     EXPECT_EQ ("a", arr->Get(5));
 }
 
-TEST(HeapSort, IntegerArray){
-    auto arr = new ArraySequence<int>({5, 2, 3, 6, 1, 4});
-    arr = (ArraySequence<int>*)SequenceSorter<int>::HeapSort(arr, [](int a, int b){ return a > b; });
-    EXPECT_EQ (1, arr->Get(0));
-    EXPECT_EQ (2, arr->Get(1));
-    EXPECT_EQ (3, arr->Get(2));
-    EXPECT_EQ (4, arr->Get(3));
-    EXPECT_EQ (5, arr->Get(4));
-    EXPECT_EQ (6, arr->Get(5));
-}
-TEST(HeapSort, FloatArray){
-    auto arr = new ArraySequence<float>({0.5f, 0.2f, 0.3f, 0.6f, 0.1f, 0.4f});
-    arr = (ArraySequence<float>*)SequenceSorter<float>::HeapSort(arr, [](float a, float b){ return a > b; });
-    EXPECT_EQ (0.1f, arr->Get(0));
-    EXPECT_EQ (0.2f, arr->Get(1));
-    EXPECT_EQ (0.3f, arr->Get(2));
-    EXPECT_EQ (0.4f, arr->Get(3));
-    EXPECT_EQ (0.5f, arr->Get(4));
-    EXPECT_EQ (0.6f, arr->Get(5));
-}
-TEST(HeapSort, StringArray){
-    auto arr = new ArraySequence<string>({"a", "g", "cow", "human", "Sergei", "programming"});
-    arr = (ArraySequence<string>*)SequenceSorter<string>::HeapSort(arr,[](const string& a, const string& b){
-        //return a.size() > b.size();
-        return a.compare(b);
-    });
-    EXPECT_EQ ("human", arr->Get(0));
-    EXPECT_EQ ("Sergei", arr->Get(1));
-    EXPECT_EQ ("g", arr->Get(2));
-    EXPECT_EQ ("cow", arr->Get(3));
-    EXPECT_EQ ("a", arr->Get(4));
-    EXPECT_EQ ("programming", arr->Get(5));
-}
-TEST(HeapSort, IntegerList){
-    auto arr = new ListSequence<int>({5, 2, 3, 6, 1, 4});
-    arr = (ListSequence<int>*)SequenceSorter<int>::HeapSort(arr, [](int a, int b){ return a > b; });
-    EXPECT_EQ (1, arr->Get(0));
-    EXPECT_EQ (2, arr->Get(1));
-    EXPECT_EQ (3, arr->Get(2));
-    EXPECT_EQ (4, arr->Get(3));
-    EXPECT_EQ (5, arr->Get(4));
-    EXPECT_EQ (6, arr->Get(5));
-}
-TEST(HeapSort, FloatList){
-    auto arr = new ListSequence<float>({0.5f, 0.2f, 0.3f, 0.6f, 0.1f, 0.4f});
-    arr = (ListSequence<float>*)SequenceSorter<float>::HeapSort(arr, [](float a, float b){ return a > b; });
-    EXPECT_EQ (0.1f, arr->Get(0));
-    EXPECT_EQ (0.2f, arr->Get(1));
-    EXPECT_EQ (0.3f, arr->Get(2));
-    EXPECT_EQ (0.4f, arr->Get(3));
-    EXPECT_EQ (0.5f, arr->Get(4));
-    EXPECT_EQ (0.6f, arr->Get(5));
-}
-TEST(HeapSort, StringList){
-    auto arr = new ListSequence<string>({"a", "g", "cow", "human", "Sergei", "programming"});
-    arr = (ListSequence<string>*)SequenceSorter<string>::HeapSort(arr,[](const string& a, const string& b){
-        //return a.size() > b.size();
-        return a.compare(b);
-    });
-    EXPECT_EQ ("human", arr->Get(0));
-    EXPECT_EQ ("Sergei", arr->Get(1));
-    EXPECT_EQ ("g", arr->Get(2));
-    EXPECT_EQ ("cow", arr->Get(3));
-    EXPECT_EQ ("a", arr->Get(4));
-    EXPECT_EQ ("programming", arr->Get(5));
-}
+//TEST(HeapSort, IntegerArray){
+//    auto arr = new ArraySequence<int>({5, 2, 3, 6, 1, 4});
+//    arr = (ArraySequence<int>*)SequenceSorter<int>::HeapSort(arr, [](int a, int b){ return a > b; });
+//    EXPECT_EQ (1, arr->Get(0));
+//    EXPECT_EQ (2, arr->Get(1));
+//    EXPECT_EQ (3, arr->Get(2));
+//    EXPECT_EQ (4, arr->Get(3));
+//    EXPECT_EQ (5, arr->Get(4));
+//    EXPECT_EQ (6, arr->Get(5));
+//}
+//TEST(HeapSort, FloatArray){
+//    auto arr = new ArraySequence<float>({0.5f, 0.2f, 0.3f, 0.6f, 0.1f, 0.4f});
+//    arr = (ArraySequence<float>*)SequenceSorter<float>::HeapSort(arr, [](float a, float b){ return a > b; });
+//    EXPECT_EQ (0.1f, arr->Get(0));
+//    EXPECT_EQ (0.2f, arr->Get(1));
+//    EXPECT_EQ (0.3f, arr->Get(2));
+//    EXPECT_EQ (0.4f, arr->Get(3));
+//    EXPECT_EQ (0.5f, arr->Get(4));
+//    EXPECT_EQ (0.6f, arr->Get(5));
+//}
+//TEST(HeapSort, StringArray){
+//    auto arr = new ArraySequence<string>({"a", "g", "cow", "human", "Sergei", "programming"});
+//    arr = (ArraySequence<string>*)SequenceSorter<string>::HeapSort(arr,[](const string& a, const string& b){
+//        //return a.size() > b.size();
+//        return a.compare(b);
+//    });
+//    EXPECT_EQ ("human", arr->Get(0));
+//    EXPECT_EQ ("Sergei", arr->Get(1));
+//    EXPECT_EQ ("g", arr->Get(2));
+//    EXPECT_EQ ("cow", arr->Get(3));
+//    EXPECT_EQ ("a", arr->Get(4));
+//    EXPECT_EQ ("programming", arr->Get(5));
+//}
+//TEST(HeapSort, IntegerList){
+//    auto arr = new ListSequence<int>({5, 2, 3, 6, 1, 4});
+//    arr = (ListSequence<int>*)SequenceSorter<int>::HeapSort(arr, [](int a, int b){ return a > b; });
+//    EXPECT_EQ (1, arr->Get(0));
+//    EXPECT_EQ (2, arr->Get(1));
+//    EXPECT_EQ (3, arr->Get(2));
+//    EXPECT_EQ (4, arr->Get(3));
+//    EXPECT_EQ (5, arr->Get(4));
+//    EXPECT_EQ (6, arr->Get(5));
+//}
+//TEST(HeapSort, FloatList){
+//    auto arr = new ListSequence<float>({0.5f, 0.2f, 0.3f, 0.6f, 0.1f, 0.4f});
+//    arr = (ListSequence<float>*)SequenceSorter<float>::HeapSort(arr, [](float a, float b){ return a > b; });
+//    EXPECT_EQ (0.1f, arr->Get(0));
+//    EXPECT_EQ (0.2f, arr->Get(1));
+//    EXPECT_EQ (0.3f, arr->Get(2));
+//    EXPECT_EQ (0.4f, arr->Get(3));
+//    EXPECT_EQ (0.5f, arr->Get(4));
+//    EXPECT_EQ (0.6f, arr->Get(5));
+//}
+//TEST(HeapSort, StringList){
+//    auto arr = new ListSequence<string>({"a", "g", "cow", "human", "Sergei", "programming"});
+//    arr = (ListSequence<string>*)SequenceSorter<string>::HeapSort(arr,[](const string& a, const string& b){
+//        //return a.size() > b.size();
+//        return a.compare(b);
+//    });
+//    EXPECT_EQ ("human", arr->Get(0));
+//    EXPECT_EQ ("Sergei", arr->Get(1));
+//    EXPECT_EQ ("g", arr->Get(2));
+//    EXPECT_EQ ("cow", arr->Get(3));
+//    EXPECT_EQ ("a", arr->Get(4));
+//    EXPECT_EQ ("programming", arr->Get(5));
+//}
 
 TEST(QuickSort, IntegerArray){
     auto arr = new ArraySequence<int>({5, 2, 3, 6, 1, 4});
--- cmake-build-debug/Tests/gtests/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/CProj/lab4/Tests/gtests/googletest
-# Build directory: D:/CProj/lab4/cmake-build-debug/Tests/gtests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
